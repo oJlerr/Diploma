89eafa50e8eedb6e7ea64a82ecf64ee861079ae9
oJlerr <59744560+oJlerr@users.noreply.github.com>
2020-06-06 16:50:01 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 
 $(function(){
+
+// Button scroll top
+
 	$(window).scroll(function(){
 		if($(window).scrollTop() > 100) {
 			$('#scroll_top').show();
@@ -7,13 +10,38 @@ $(function(){
 			$('#scroll_top').hide();
 		}
 	});
- 
+
+
+// preloader
+
+$(window).on('load', function () {
+    var $preloader = $('#page-preloader'),
+        $spinner   = $preloader.find('.spinner');
+    $spinner.fadeOut();
+    $preloader.delay(350).fadeOut('slow');
+});
+
+// scroll
 	$('#scroll_top').click(function(){
 		$('html, body').animate({scrollTop: 0}, 600);
 		return false;
 	});
+
+// hamburger
 	 $('.burger').click(function () {
         $('.menu-collapse').toggleClass('d-none').css('order', '1');
         $('.menu').toggleClass('menu-opened');
 	})
-});
+//Slider
+
+	 $('.slider').slick({
+        dots: true,
+        infinite: false,
+        autoplay: true,
+        prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-angle-left" aria-hidden="true"></i></button>',
+        nextArrow: '<button type="button" class="slick-next"><i class="fa fa-angle-right"></i></button>',
+    });
+
+
+ 
+	});	
